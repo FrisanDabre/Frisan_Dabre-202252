66f1a7644d6dcc564ac276dcb2ad1563837dc28c
FrisanDabre <86957438+FrisanDabre@users.noreply.github.com>
2021-07-05 16:59:10 +0530

    Update Code.c

--- Code.c
+++ Code.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 main() {
-	FILE *fp:
-	fp[D[D[A[C[C[C[C[C[C[C[C[;[B[D[D[D[D[D[D[D[D[D
-[D[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
-[C
-[A[D[C[C[C[C[C[C[C[C[C[C
+	FILE *fp;
+	
 	fp = fopen("/tmp/test.txt", "w+");
 	fprintf(fp, "This is testing for fprintf...\n");
 	fputs("This is testing for fputs...\n",fp);
 	fclose(fp);
 }
-^C
+
